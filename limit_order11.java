b010e2b47e47a5d7d93d448e90f9f593bfee56df
naoki hashimoto <hashimoto27ban@yahoo.co.jp>
2017-01-14 23:37:08 +0900

    大幅変更

--- limit_order11.java
+++ limit_order11.java
@@ -16,7 +16,7 @@ public class limit_order11{
 
         while((txtFileName = br.readLine()) != null) {
 
-        	String Index;//asdas
+        	String Index;
         	String number[] = new String[5000000];
         	String a;
         	int n = 0;
@@ -52,11 +52,11 @@ public class limit_order11{
             int day_number = 0;
             String bid_volume;//最良買気配の累積枚数
             int bid_volume1 = 0;//最良買気配の累積枚数
-            int bid_volume2 = 0;
+            int bid_volume2 = 0;//1つ前の最良買気配の累積枚数
             int bid_volume_dif = 0;//前の買いの指値注文からの増減
-            String ask_volume;//最良売気配の累積枚数
+            String ask_volume = null;//最良売気配の累積枚数
             int ask_volume1 = 0;//最良売気配の累積枚数
-            int ask_volume2 = 0;
+            int ask_volume2 = 0;//1つ前の最良売気配の累積枚数
             int ask_volume_dif = 0;//前の売りの指値注文からの増減
             String trade_price;
             int trade_price1 = 0;
@@ -82,6 +82,10 @@ public class limit_order11{
             int bid_volume_first = 0;
             int ask_volume_first = 0;
             String time_first = null;
+            
+            double trade_time = 0.0;//約定の時間（計算結果）
+            double trade_price2 = 0.0;//約定価格
+            int trade_volume2 = 0;
 
 
             //for文の部分
@@ -100,7 +104,7 @@ public class limit_order11{
             BufferedReader brtxt = new BufferedReader(fr);
             String line ="";
 
-            String[] filename = txtFileName.split(".");
+            String[] filename = txtFileName.split("¥¥.");
 
          	File file = new File(filename[0] + "_limit_order.txt");
          	PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
@@ -108,21 +112,25 @@ public class limit_order11{
             while ((line = brtxt.readLine()) != null) {
 
             	Index = line;
+            	//System.out.println(Index);
+            	//System.out.println(Index.substring(0,1));
+            	
             	i2 = Index.length();
-            	//if(i2>13){
-            	day = line.substring(6,14);//年月日
+            	
+            	if(!(Index.substring(0,1).equals("#"))){
+            		
+            	day = line.substring(12,20);//年月日
             	day_number = Integer.parseInt(day);//年月日
-            	time = line.substring(15,27);//時間
-            	transaction = line.substring(28,33);//Quote or Trade
-        		String c1 = Index.substring(15,17);//時
-        		String c2 = Index.substring(18,20);//分
-        		String c3 = Index.substring(21,27);//秒
+            	time = line.substring(21,36);//時間
+            	transaction = line.substring(37,42);//Quote or Trade
+        		String c1 = Index.substring(21,23);//時
+        		String c2 = Index.substring(24,26);//分
+        		String c3 = Index.substring(27,36);//秒
         		time1 = Double.parseDouble(c1);//時
         		time2 = Double.parseDouble(c2);//分
         		time3 = Double.parseDouble(c3);//秒
         		time_total1 = time1*3600 + time2*60 + time3;//時間を秒で表示
             	
-            	//}
             	if(!(day.equals(day1))){
             		if(count14 != 0){
                 		//for(int i = 0;i<for_array;i++){
@@ -139,6 +147,8 @@ public class limit_order11{
             			count_dif[i]=0;
             		}
             		number1 = 0;
+            		bid_volume1 = 0;
+            		ask_volume1 = 0;
                 	bid1[0]=0;
                 	ask1[0]=0;
                 	bid1[1]=0;
@@ -174,6 +184,7 @@ public class limit_order11{
                 for(i1=0;i1<i2;i1++){
                     a = Index.substring(i1,i1+1);
                     if(count == 5 && !(a.equals(",")) && count1 == 0){
+                    	
                     	i7 = i1;
                     	count1++;
                     }
@@ -228,21 +239,26 @@ public class limit_order11{
                         count10++;
                         i4 = i1-1;
                         ask=Index.substring(i3,i4);//最良売気配の値段
-                        if(ask.equals(",")){
+                        /*if(ask.equals(",")){
                             ask1[number1] = 0;
-                        }
-                        else{
+                        }*/
+                        //else{
+                        	//if()
                             ask1[number1] = Integer.parseInt(ask);
-                        }
+                        //}
                     }
                     if(count == 11 && !(a.equals(",")) && count11 == 0){
                     	i5 = i1;
                     	count11++;
                     }
-                    if(count == 12 && count11 != 0 && count12 == 0){
+                    if((count == 12 && count11 != 0 && count12 == 0) || (count == 11 && count11 != 0 && i1 ==(i2 - 1))){
                     	i6 = i1-1;
+                    	if(i1 == (i2 - 1)){
+                    		i6 = i1;
+                    	}
                     	ask_volume = Index.substring(i5,i6);//最良売気配の累積枚数
                     	ask_volume1 =Integer.parseInt(ask_volume);
+                    	//System.out.println(Index + "&" + ask_volume1);
                     	count12++;
                     }
                     if(a.equals(",")){
@@ -250,17 +266,23 @@ public class limit_order11{
                     }
                 }
                 if(bid1[1]>ask1[1] && bid1[1] != 0 && ask1[1] != 0){//意味の分からないことが起きていないか確認
-                													//
+                													
                 	System.out.println(day + " " + time + " " + bid1[1] + " " + ask1[1]);
                 }
+                if(transaction.equals("Trade")){
+                	trade_time = time_total1;
+                	//trade_volume2 = trade_volume1;
+                }
 
-                if(bid1[0] != ask1[0] && count13 == 0){//寄り付きが終了したときの初期値の設定
+                if(bid1[0] != ask1[0] && ask1[0] != 0 && bid1[0] != 0 && count13 == 0){//寄り付きが終了したときの初期値の設定
                 	count13++;
                 	number1++;
                 	day1 = day;
                 	bid_volume2 = bid_volume1;
                 	ask_volume2 = ask_volume1;
                 	time_total2 = time_total1;
+                	System.out.println(day + " " + bid_volume2 + " " + ask_volume2 );
+                	//System.out.println(Index + " " + ask_volume + " " + ask1[0]);
                 }
                 /*else if((bid1[0] != bid1[1] || ask1[0] != ask1[1]) && count13 != 0 && bid1[1] != 0 && ask1[1] != 0 && bid1[1] != ask1[1]){//板が移動したことを認知するプログラム
                 	time_first = time;
@@ -281,46 +303,95 @@ public class limit_order11{
                 else if(count13 != 0){//共通
                 	time_dif = time_total1 - time_total2;
 
-                		if(bid_volume1 != 0 && ask_volume1 == 0){//実は意味ない(古いデータのときには必要だった)
-                    		bid_volume_dif = bid_volume1 - bid_volume2;
-                    		bid_volume2 += bid_volume_dif;
+                		//if(bid_volume1 != 0 && ask_volume1 == 0){//実は意味ない(古いデータのときには必要だった)
+                    		//bid_volume_dif = bid_volume1 - bid_volume2;
+                    		//bid_volume2 += bid_volume_dif;
 
-                    	}
-                		else if(bid_volume1 == 0 && ask_volume1 != 0){//実は意味ない(古いデータのときには必要だった)
-                    		ask_volume_dif = ask_volume1 - ask_volume2;
-                    		ask_volume2 += ask_volume_dif;
-
-                    	}
-                		else if(bid_volume1 != 0 && ask_volume1 != 0 && !(transaction.equals("Trade"))){
-                    		bid_volume_dif = bid_volume1 - bid_volume2;
-                    		if(bid_volume_dif <= 0){
+                    	//}
+                		//else if(bid_volume1 == 0 && ask_volume1 != 0){//実は意味ない(古いデータのときには必要だった)
+                    		//ask_volume_dif = ask_volume1 - ask_volume2;
+                    		//ask_volume2 += ask_volume_dif;
+                		//}
+                
+                
+                    	
+                		if(bid_volume1 != 0 && ask_volume1 != 0 && !(transaction.equals("Trade"))){
+                			//System.out.println(time_total1);
+                    		if(trade_time == time_total1 && trade_price1 == bid1[1]){
+                    			if(trade_volume1 > (bid_volume2 - bid_volume1)){
+                    				bid_volume_dif = trade_volume1 - (bid_volume2 - bid_volume1);
+                    			}
+                    			else{
+                    				bid_volume_dif = 0;
+                    			}
+                    		}
+                    		else if(trade_volume1 == bid_volume2){//板の移動
+                    			bid_volume_dif = 0;
+                    		}
+                    		else{
+                    			bid_volume_dif = bid_volume1 - bid_volume2;
+                    		}
+                    		bid_volume2 = bid_volume1;
+                    		
+                    		/*if(bid_volume_dif <= 0){
                     			bid_volume2 = bid_volume1;
                     		}
                     		else{
                     			bid_volume2 += bid_volume_dif;
                     			bid_volume_dif_total +=bid_volume_dif;
+                    		}*/
+                    		
+                    		
+                    		if(trade_time == time_total1 && trade_price1 == ask1[1]){
+                    			if((trade_volume1 > (ask_volume2 - ask_volume1))){//指値注文
+                    				ask_volume_dif = trade_volume1 - (ask_volume2 - ask_volume1);
+                    				if(day.equals("20060105")){
+                    					System.out.println(trade_volume1 + " " + ask_volume2 + " " + ask_volume1);
+                    					System.out.println(Index);
+                    				}
+                    				
+                    			}
+                    			else{//注文の取り消しとか
+                    				ask_volume_dif = 0;
+                    			}
                     		}
-                    		ask_volume_dif = ask_volume1 - ask_volume2;
-                    		if(ask_volume_dif <= 0){
+                    		else if(trade_volume1 == ask_volume2){//板の移動
+                    			ask_volume_dif = 0;
+                    		}
+                    		else{
+                    			ask_volume_dif = ask_volume1 - ask_volume2;
+                    		}
+                    		
+                    		
+                    		ask_volume2 = ask_volume1;
+
+                    		/*if(ask_volume_dif <= 0){
                     			ask_volume2 = ask_volume1;
                     		}
                     		else{
                     			ask_volume2 += ask_volume_dif;
                     			ask_volume_dif_total += ask_volume_dif;
-                    		}
+                    		}*/
                     		volume_dif = bid_volume_dif + ask_volume_dif;//なぜ足しているのか謎
                     		/*if(volume_dif > 0){
                     			pw.println(day + " " + time + " " + volume_dif);//指値注文の書き込み
                     		}*/
+                    		int q1 = 0;
+                    		int q2 = 0;
                     		if(bid_volume_dif > 0){
-                    			pw.println(day + " " + time + " " + volume_dif + " bid");//指値買注文の書き込み
+                    			pw.println(day + " " + time + " " + bid_volume_dif + " bid");//指値買注文の書き込み
+                    			q1++;
                     		}
                     		else if(ask_volume_dif > 0){
-                    			pw.println(day + " " + time + " " + volume_dif + " ask");//指値売注文の書き込み
+                    			pw.println(day + " " + time + " " + ask_volume_dif + " ask");//指値売注文の書き込み
+                    			q2++;
+                    		}
+                    		if(q1 == 1 && q2 == 1){
+                    			System.out.println("なんてこった。ぱんたこった。");
                     		}
 
                     	}
-                		else if(transaction.equals("Trade")){
+                		/*else if(transaction.equals("Trade")){
                 			BigDecimal x4 = new BigDecimal(time_dif);
                     		double time_dif_x = x4.setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
                     		if(count_pw == 0 && (bid_volume_dif_total != 0 || ask_volume_dif_total != 0)){
@@ -332,13 +403,14 @@ public class limit_order11{
                 			count14 = 0;
                 			bid_volume_dif_total = 0;
                 			ask_volume_dif_total = 0;
-                    		if(trade_price1 == bid1[0]){
+                    		if(trade_price1 == bid1[number1]){//約定の分、板の枚数を少なくする。
                     			bid_volume2 -= trade_volume1;
                     		}
-                    		else if(trade_price1 == ask1[0]){
+                    		else if(trade_price1 == ask1[number1]){//約定の分、板の枚数を少なくする。
                     			ask_volume2 -= trade_volume1;
                     		}
-                		}
+                		}*/
+                
                 }
 
 
@@ -364,6 +436,7 @@ public class limit_order11{
                 i9 = 0;
                 i10 = 0;
                 n++;
+            	}
 
 
             }
